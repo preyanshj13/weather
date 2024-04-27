bbcc3b88337edbc022065e95d8cdcde412f856a1
Preyansh <Preyansh.jain13@gmail.com>
2024-04-27 18:11:59 +0530

    header link changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <a href='/' className='text-3xl flex justify-center text-center pt-5 font-bold'>Weather App</a>
+    <a href='/weather' className='text-3xl flex justify-center text-center pt-5 font-bold'>Weather App</a>
   )
 }
 
